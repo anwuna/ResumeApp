ae569955b461e6b52ad82b9663dc64905093dfbb
John Anwuna <kevinagentz@yahoo.com>
2019-06-20 23:26:41 -0600

    add more ui tests

--- ResumeAppUITests/ResumeAppUITests.swift
+++ ResumeAppUITests/ResumeAppUITests.swift
@@ -18,8 +18,7 @@ class ResumeAppUITests: XCTestCase {
         application.launch()
     }
 
-    func testHeadersExists() {
-        
+    func test_headersExists() {
         let tablesQuery = XCUIApplication().tables
         XCTAssertTrue(tablesQuery.staticTexts["NAME"].exists)
         XCTAssertTrue(tablesQuery.staticTexts["SUMMARY"].exists)
@@ -29,29 +28,68 @@ class ResumeAppUITests: XCTestCase {
         
     }
     
-    func testResumeNameIs_ChibunduAnwuna() {
-        
+    func test_resumeName_Exists() {
         let tablesQuery = application.tables
-        XCTAssertTrue(tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Mr. Chibundu John Anwuna"]/*[[".cells.staticTexts[\"Mr. Chibundu John Anwuna\"]",".staticTexts[\"Mr. Chibundu John Anwuna\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists)
+        XCTAssertTrue(tablesQuery.staticTexts["Mr. Chibundu John Anwuna"].exists)
     }
     
-    func testSummaryContainsCertainText() {
+    func test_summaries_Exists() {
         let paragraph1 = "An Ingenious and passionate software"
         let paragraph2 = "Extensive knowledge in software development"
         let paragraph3 = "Excellent troubleshooting skills, debugging"
 
         for pargs in [paragraph1, paragraph2, paragraph3] {
             let elementQuery = application.staticTexts.containing(NSPredicate(format: "label CONTAINS[c] %@", pargs))
-            XCTAssertGreaterThanOrEqual(elementQuery.count, 1)
+            XCTAssertEqual(elementQuery.count, 1)
         }
     }
     
-    func testCompanyNames_Exists(){
+    func test_skills_Exists() {
+        let skills = "C#, Swift, Ruby, ASP.NET, JavaScript"
+        let skillsElementQuery = application.staticTexts.containing(NSPredicate(format: "label CONTAINS[c] %@", skills))
+        XCTAssertEqual(skillsElementQuery.count, 1)
+    }
+    
+    func test_companyNames_Exists(){
         let tableQuery = application.tables
         XCTAssertTrue(tableQuery.staticTexts["SKF Calgary"].exists)
         XCTAssertTrue(tableQuery.staticTexts["SystemSpecs, Nigeria"].exists)
         XCTAssertTrue(tableQuery.staticTexts["Interswitch Group"].exists)
         XCTAssertTrue(tableQuery.staticTexts["MindWorks Solutions Limited "].exists)
         
+    }
+    
+    func test_education_Exists() {
+        let education = "University of Lagos"
+        let educationElementQuery = application.staticTexts.containing(NSPredicate(format: "label CONTAINS[c] %@", education))
+        XCTAssertEqual(educationElementQuery.count, 1)
+    }
+    
+    func test_refreshButton_Exists() {
+        XCTAssertTrue(application.navigationBars["RESUME"].buttons["Refresh"].exists)
+    }
+    
+    func test_canRefresh_TableView() {
+        let app = XCUIApplication()
+        app.navigationBars["RESUME"].buttons["Refresh"].tap()
+        
+        let tablesQuery = app.tables
+        XCTAssertTrue(tablesQuery.staticTexts["Mr. Chibundu John Anwuna"].exists)
+        
+        let summary = "An Ingenious and passionate software"
+        let summaryElementQuery = application.staticTexts.containing(NSPredicate(format: "label CONTAINS[c] %@", summary))
+        XCTAssertEqual(summaryElementQuery.count, 1)
+        
+        let skills = "C#, Swift, Ruby, ASP.NET, JavaScript"
+        let skillsElementQuery = application.staticTexts.containing(NSPredicate(format: "label CONTAINS[c] %@", skills))
+        XCTAssertEqual(skillsElementQuery.count, 1)
+        XCTAssertTrue(tablesQuery.staticTexts["SKF Calgary"].exists)
+        XCTAssertTrue(tablesQuery.staticTexts["SystemSpecs, Nigeria"].exists)
+        XCTAssertTrue(tablesQuery.staticTexts["Interswitch Group"].exists)
+        XCTAssertTrue(tablesQuery.staticTexts["MindWorks Solutions Limited "].exists)
+        
+        let education = "University of Lagos"
+        let educationElementQuery = application.staticTexts.containing(NSPredicate(format: "label CONTAINS[c] %@", education))
+        XCTAssertEqual(educationElementQuery.count, 1)
     }
 }
